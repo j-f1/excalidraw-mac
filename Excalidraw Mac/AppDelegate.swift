acd3fc8822976981546a12f19ff5bad4c058e059
Jed Fox <git@twopointzero.us>
2020-03-25 08:37:40 -0400

    Initial Commit

---
+++ Excalidraw Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Excalidraw Mac
+//
+//  Created by Jed Fox on 3/25/20.
+//  Copyright © 2020 Jed Fox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Excalidraw Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Excalidraw Mac
+//
+//  Created by Jed Fox on 3/25/20.
+//  Copyright © 2020 Jed Fox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
